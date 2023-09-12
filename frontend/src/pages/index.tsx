2a90905a7296625b174f81d3b85a1598047ae964
Grayson-Remington <graysonbremington@gmail.com>
2023-09-11 23:42:35 -0400

    Initial commit

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,393 @@
+import { useEffect, useRef, useState } from 'react';
+import io from 'socket.io-client';
+import { motion, useAnimation } from 'framer-motion';
+
+const Chat = () => {
+	const [roomId, setRoomId] = useState();
+	const [playerName, setPlayerName] = useState();
+	const [connectedToRoom, setConnectedToRoom] = useState(false);
+	const [currentCard, setCurrentCard] = useState();
+	const [nextCard, setNextCard] = useState();
+	const [thirdCard, setThirdCard] = useState();
+	const [turnScore, setTurnScore] = useState(0);
+	const [totalScore, setTotalScore] = useState(0);
+	const [correct, setCorrect] = useState(null);
+	const [deathStack, setDeathStack] = useState(0);
+	const [socket, setSocket] = useState(null);
+	const [turn, setTurn] = useState(undefined);
+	const turnRef = useRef();
+	const playerNameRef = useRef();
+	const currentCardRef = useRef();
+	const nextCardRef = useRef();
+	const thirdCardRef = useRef();
+	const turnScoreRef = useRef(0);
+	const deathStackRef = useRef(0);
+	const [isFlipped, setIsFlipped] = useState(false);
+	const controls = useAnimation();
+	const controls2 = useAnimation();
+	const [isAnimating, setIsAnimating] = useState(false);
+	const handleFlip = async () => {
+		await controls.start({
+			x: 150,
+			rotateY: 0,
+			zIndex: 0,
+			transition: { duration: 0.5 },
+		});
+
+		// Snap back to the original position
+		await controls.start({
+			x: 0,
+			rotateY: 0,
+			zIndex: 20,
+			transition: { duration: 0.5 },
+		});
+		await controls.start({
+			x: 0,
+			rotateY: 180,
+			zIndex: 0,
+			transition: { duration: 0 },
+		});
+		setIsFlipped(false);
+		setIsAnimating(false);
+	};
+	const handleFlip2 = async () => {
+		await controls.start({
+			x: 150,
+			rotateY: 0,
+			zIndex: 0,
+			transition: { duration: 0.5 },
+		});
+		await controls2.start({
+			x: 300,
+			rotateY: 0,
+			zIndex: 0,
+			transition: { duration: 0.5 },
+		});
+
+		// Snap back to the original position
+		await controls.start({
+			x: 0,
+			rotateY: 0,
+			zIndex: 0,
+			transition: { duration: 0.5 },
+		});
+		await controls2.start({
+			x: 0,
+			rotateY: 0,
+			zIndex: 20,
+			transition: { duration: 0.5 },
+		});
+		await controls.start({
+			x: 0,
+			rotateY: 180,
+			zIndex: 0,
+			transition: { duration: 0 },
+		});
+		await controls2.start({
+			x: 0,
+			rotateY: 180,
+			zIndex: 0,
+			transition: { duration: 0 },
+		});
+		setIsFlipped(false);
+		setIsAnimating(false);
+	};
+	useEffect(() => {
+		turnRef.current = turn;
+		playerNameRef.current = playerName;
+		currentCardRef.current = currentCard;
+		nextCardRef.current = nextCard;
+		thirdCardRef.current = thirdCard;
+		turnScoreRef.current = turnScore;
+		deathStackRef.current = deathStack;
+	}, [
+		turn,
+		playerName,
+		nextCard,
+		currentCard,
+		turnScore,
+		deathStack,
+		thirdCard,
+	]);
+	useEffect(() => {
+		console.log('Connecting to WebSocket server...');
+		const newSocket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL, {
+			transports: ['websocket'],
+		});
+
+		newSocket.on('connect', () => {
+			console.log('Connected to WebSocket server');
+		});
+		newSocket.on('playerJoined', () => {
+			setConnectedToRoom(true);
+		});
+		newSocket.on('currentCard', (currentCard) => {
+			setCurrentCard(currentCard);
+		});
+		newSocket.on('nextCard', (nextCard) => {
+			setNextCard(nextCard);
+		});
+		newSocket.on('thirdCard', (thirdCard) => {
+			setThirdCard(thirdCard);
+		});
+		newSocket.on('deathStack', (deathStack) => {
+			setDeathStack(deathStack);
+		});
+		newSocket.on('turnScore', (turnScore) => {
+			setTurnScore(turnScore);
+		});
+		newSocket.on('guess', (guess) => {
+			if (guess == 'purple') {
+				handleFlip2();
+				setTimeout(() => {
+					setIsFlipped(true);
+				}, 150);
+				setTimeout(() => {
+					const thirdCard = thirdCardRef.current;
+
+					setCurrentCard(thirdCard);
+				}, 2000);
+				setTimeout(() => {
+					const thirdCard = thirdCardRef.current;
+
+					setCurrentCard(thirdCard);
+				}, 2000);
+			} else {
+				handleFlip();
+				setTimeout(() => {
+					setIsFlipped(true);
+				}, 300);
+				setTimeout(() => {
+					const nextCard = nextCardRef.current;
+
+					setCurrentCard(nextCard);
+				}, 1000);
+			}
+		});
+		newSocket.on('correct', (correct) => {
+			console.log('Received correct:', correct);
+			setCorrect(correct);
+			setIsAnimating(true);
+
+			const currentTurn = turnRef.current;
+			const currentPlayerName = playerNameRef.current;
+			const currentTurnScore = turnScoreRef.current;
+			const currentDeathStack = deathStackRef.current;
+			if (
+				currentTurn &&
+				currentTurn === currentPlayerName &&
+				correct === 'purpleTrue'
+			) {
+				// Increase the score
+
+				setTurnScore((prevTurnScore) => prevTurnScore + 1);
+				setDeathStack((prevDeathStack) => prevDeathStack + 1);
+			}
+			if (
+				currentTurn &&
+				currentTurn === currentPlayerName &&
+				correct === 'false'
+			) {
+				setTotalScore(
+					(prevTotalScore) => prevTotalScore - currentDeathStack - 1
+				);
+
+				setDeathStack(0);
+				setTurnScore(0);
+			}
+			if (
+				currentTurn &&
+				currentTurn === currentPlayerName &&
+				correct === 'purpleFalse'
+			) {
+				setTotalScore(
+					(prevTotalScore) => prevTotalScore - currentDeathStack - 2
+				);
+
+				setDeathStack(0);
+				setTurnScore(0);
+			}
+		});
+		newSocket.on('turn', (turn) => {
+			setTurn(turn);
+		});
+
+		newSocket.on('playerLeft', (playerId) => {
+			// Handle a player leaving
+			console.log(`Player with ID ${playerId} has left the room.`);
+		});
+		setSocket(newSocket);
+
+		// Clean up the socket connection on unmount
+		return () => {
+			console.log('Disconnecting from WebSocket server...');
+			newSocket.disconnect();
+		};
+	}, []);
+
+	const handleJoinRoom = (roomId, playerName) => {
+		if (socket) {
+			socket.emit('joinRoom', {
+				requestedRoomId: roomId,
+				playerName: playerName,
+			});
+		}
+	};
+
+	const handleGuess = (guess) => {
+		if (socket) {
+			socket.emit('guess', guess);
+		}
+	};
+	const handlePass = (turnScore) => {
+		if (socket) {
+			socket.emit('pass', turnScore);
+			setTurnScore(0);
+
+			setTotalScore(
+				(prevTotalScore) => prevTotalScore + turnScoreRef.current
+			);
+		}
+	};
+	return (
+		<div className='flex flex-col items-center p-4 gap-4'>
+			<h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl '>
+				Purple
+			</h1>
+
+			{!connectedToRoom ? (
+				<div className='max-w-4xl flex flex-col'>
+					<h1>RoomId</h1>
+					<input
+						className='border border-black'
+						type='input'
+						onChange={(e) => setRoomId(e.target.value)}
+					/>
+					<h1>Username</h1>
+					<input
+						className='border border-black'
+						type='input'
+						onChange={(e) => setPlayerName(e.target.value)}
+					/>
+					<button onClick={() => handleJoinRoom(roomId, playerName)}>
+						Join
+					</button>
+				</div>
+			) : (
+				<>
+					<div className='flex text-center gap-4'>
+						<div className='font-bold'>
+							<h1>Total Score</h1>
+							<h1 className=' text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl'>
+								{totalScore}
+							</h1>
+						</div>
+
+						<div className='font-bold'>
+							<h1>Turn Score</h1>
+							<h1 className=' text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl'>
+								{turnScore}
+							</h1>
+						</div>
+						<div className='font-bold'>
+							<h1>Death Stack</h1>
+							<h1 className=' text-3xl font-semibold tracking-tight text-red-600 sm:text-5xl'>
+								{deathStack}
+							</h1>
+						</div>
+					</div>
+					<div className='flex relative gap-4'>
+						<div className='z-10 flashcard'>
+							<div className='front'>
+								<img
+									className=' h-44'
+									src={`./${currentCard}.svg`}
+									alt=''
+								/>
+							</div>
+						</div>
+						<motion.div
+							className='absolute h-44'
+							initial={{ rotateY: 180, x: 0 }}
+							animate={controls}
+						>
+							<div className='front'>
+								{isFlipped ? (
+									<img
+										className=' h-44'
+										src={`./${nextCard}.svg`}
+										alt=''
+									/>
+								) : (
+									<img
+										className=' h-44'
+										src={`./blue2.svg`}
+										alt=''
+									/>
+								)}
+							</div>
+						</motion.div>
+						<motion.div
+							className='absolute h-44'
+							initial={{ rotateY: 180, x: 0 }}
+							animate={controls2}
+						>
+							<div className='front'>
+								{isFlipped ? (
+									<img
+										className=' h-44'
+										src={`./${thirdCard}.svg`}
+										alt=''
+									/>
+								) : (
+									<img
+										className=' h-44'
+										src={`./blue2.svg`}
+										alt=''
+									/>
+								)}
+							</div>
+						</motion.div>
+					</div>
+
+					{turn && turn == playerName && (
+						<div className='flex gap-4 p-4'>
+							<button
+								onClick={() => handleGuess('lower')}
+								disabled={isAnimating}
+								className='disabled:bg-red-500 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'
+							>
+								Lower
+							</button>
+							<button
+								onClick={() => handleGuess('higher')}
+								disabled={isAnimating}
+								className='disabled:bg-red-500 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'
+							>
+								Higher
+							</button>
+							<button
+								onClick={() => handleGuess('purple')}
+								disabled={isAnimating}
+								className='disabled:bg-red-500 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'
+							>
+								Purple
+							</button>
+							{turnScore >= 3 && (
+								<button
+									onClick={() => handlePass(deathStack)}
+									disabled={isAnimating}
+									className='disabled:bg-red-500 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'
+								>
+									Pass
+								</button>
+							)}
+						</div>
+					)}
+				</>
+			)}
+		</div>
+	);
+};
+
+export default Chat;
---
+++ server/server.js
@@ -0,0 +1,294 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(cors()); // Enable CORS for all routes
+
+const suites = ['diamonds', 'hearts', 'spades', 'clubs'];
+
+function getRandomSuite() {
+	const randomIndex = Math.floor(Math.random() * suites.length);
+	return suites[randomIndex];
+}
+
+class Room {
+	constructor(roomId) {
+		this.roomId = roomId;
+		this.players = [];
+		this.currentPlayerIndex = 0;
+		this.currentNumber = Math.floor(Math.random() * 12) + 1;
+		this.currentSuite = getRandomSuite();
+		this.nextNumber = Math.floor(Math.random() * 12) + 1;
+		this.nextSuite = getRandomSuite();
+		this.thirdNumber = Math.floor(Math.random() * 12) + 1;
+		this.thirdSuite = getRandomSuite();
+		this.deathStack = 0;
+		this.turnScore = 0;
+	}
+
+	addPlayer(player) {
+		this.players.push(player);
+	}
+
+	removePlayer(playerId) {
+		const index = this.players.findIndex(
+			(player) => player.id === playerId
+		);
+		if (index !== -1) {
+			this.players.splice(index, 1);
+		}
+	}
+}
+
+// Store rooms in an object
+const rooms = {};
+
+io.on('connection', (socket) => {
+	console.log('A user connected');
+	socket.on('joinRoom', (data) => {
+		const { requestedRoomId, playerName } = data;
+
+		if (!rooms[requestedRoomId]) {
+			rooms[requestedRoomId] = new Room(requestedRoomId);
+		}
+		// Join the specified room
+		socket.join(requestedRoomId);
+		socket.roomId = requestedRoomId;
+
+		// Store player information (you can use an array, object, or database)
+		const player = { id: socket.id, name: playerName, score: 0 };
+		// Example: store in an array
+		io.to(requestedRoomId).emit(
+			'currentCard',
+			rooms[requestedRoomId].currentSuite +
+				'_' +
+				rooms[requestedRoomId].currentNumber.toString()
+		);
+		io.to(requestedRoomId).emit(
+			'nextCard',
+			rooms[requestedRoomId].nextSuite +
+				'_' +
+				rooms[requestedRoomId].nextNumber.toString()
+		);
+		io.to(requestedRoomId).emit(
+			'thirdCard',
+			rooms[requestedRoomId].thirdSuite +
+				'_' +
+				rooms[requestedRoomId].thirdNumber.toString()
+		);
+		rooms[requestedRoomId].addPlayer(player);
+		io.to(requestedRoomId).emit(
+			'turn',
+			rooms[requestedRoomId].players[
+				rooms[requestedRoomId].currentPlayerIndex
+			].name
+		);
+
+		// Emit an event to the room to update all clients about the new player
+		io.to(requestedRoomId).emit('playerJoined', player.name);
+		console.log(rooms);
+		console.log(requestedRoomId);
+	});
+	socket.on('deathStack', (deathStack) => {
+		let roomId = socket.roomId;
+
+		rooms[roomId].deathStack = deathStack;
+	});
+	socket.on('turnScore', (turnScore) => {
+		let roomId = socket.roomId;
+
+		rooms[roomId].turnScore = turnScore;
+	});
+	socket.on('guess', (guess) => {
+		let roomId = socket.roomId;
+		let thirdNumber = rooms[roomId].thirdNumber;
+		let thirdSuite = rooms[roomId].thirdSuite;
+		let nextNumber = rooms[roomId].nextNumber;
+		let nextSuite = rooms[roomId].nextSuite;
+		let currentNumber = rooms[roomId].currentNumber;
+		let currentSuite = rooms[roomId].currentSuite;
+		let currentPlayerIndex = rooms[roomId].currentPlayerIndex;
+		let players = rooms[roomId].players;
+		let deathStack = rooms[roomId].deathStack;
+		let turnScore = rooms[roomId].turnScore;
+		console.log(nextNumber, thirdNumber);
+		if (
+			(guess == 'purple' &&
+				(nextSuite == 'diamonds' || nextSuite == 'hearts') &&
+				(thirdSuite == 'clubs' || thirdSuite == 'spades')) ||
+			((nextSuite == 'clubs' || nextSuite == 'spades') &&
+				(thirdSuite == 'diamonds' || thirdSuite == 'hearts'))
+		) {
+			console.log('purple');
+			console.log('deathstack', deathStack);
+			console.log('score', players[currentPlayerIndex].score);
+			rooms[roomId].currentNumber = thirdNumber;
+			rooms[roomId].currentSuite = thirdSuite;
+			rooms[roomId].deathStack = deathStack + 2;
+			rooms[roomId].turnScore = turnScore + 2;
+			rooms[roomId].nextNumber = Math.floor(Math.random() * 12) + 1;
+			rooms[roomId].nextSuite = getRandomSuite();
+			rooms[roomId].thirdNumber = Math.floor(Math.random() * 12) + 1;
+			rooms[roomId].thirdSuite = getRandomSuite();
+			io.to(roomId).emit('correct', 'purpleTrue');
+			io.to(roomId).emit(
+				'currentCard',
+				currentSuite + '_' + currentNumber.toString()
+			);
+			io.to(roomId).emit(
+				'nextCard',
+				nextSuite + '_' + nextNumber.toString()
+			);
+			io.to(roomId).emit(
+				'thirdCard',
+				thirdSuite + '_' + thirdNumber.toString()
+			);
+			players[currentPlayerIndex].score =
+				players[currentPlayerIndex].score + 2;
+			io.to(roomId).emit('turn', players[currentPlayerIndex].name);
+			io.to(roomId).emit('turnScore', rooms[roomId].turnScore);
+			io.to(roomId).emit('deathStack', rooms[roomId].deathStack);
+		} else if (guess == 'purple') {
+			rooms[roomId].currentNumber = thirdNumber;
+			rooms[roomId].currentSuite = thirdSuite;
+			rooms[roomId].deathStack = 0;
+			rooms[roomId].turnScore = 0;
+			rooms[roomId].nextNumber = Math.floor(Math.random() * 12) + 1;
+			rooms[roomId].nextSuite = getRandomSuite();
+			rooms[roomId].thirdNumber = Math.floor(Math.random() * 12) + 1;
+			rooms[roomId].thirdSuite = getRandomSuite();
+			io.to(roomId).emit('correct', 'purpleFalse');
+			io.to(roomId).emit(
+				'currentCard',
+				currentSuite + '_' + currentNumber.toString()
+			);
+			io.to(roomId).emit(
+				'nextCard',
+				nextSuite + '_' + nextNumber.toString()
+			);
+			io.to(roomId).emit(
+				'thirdCard',
+				thirdSuite + '_' + thirdNumber.toString()
+			);
+			io.to(roomId).emit('deathStack', 0);
+			io.to(roomId).emit('turnScore', 0);
+			if (currentPlayerIndex == players.length - 1) {
+				rooms[roomId].currentPlayerIndex = 0;
+			} else {
+				rooms[roomId].currentPlayerIndex += 1;
+			}
+
+			io.to(roomId).emit(
+				'turn',
+				players[rooms[roomId].currentPlayerIndex].name
+			);
+		}
+		if (
+			(guess == 'higher' && currentNumber < nextNumber) |
+			(guess == 'lower' && currentNumber > nextNumber)
+		) {
+			rooms[roomId].currentNumber = nextNumber;
+			rooms[roomId].currentSuite = nextSuite;
+			rooms[roomId].deathStack = deathStack + 1;
+			rooms[roomId].turnScore = turnScore + 1;
+			rooms[roomId].nextNumber = Math.floor(Math.random() * 12) + 1;
+			rooms[roomId].nextSuite = getRandomSuite();
+			rooms[roomId].thirdNumber = Math.floor(Math.random() * 12) + 1;
+			rooms[roomId].thirdSuite = getRandomSuite();
+			io.to(roomId).emit('correct', 'true');
+			io.to(roomId).emit(
+				'currentCard',
+				currentSuite + '_' + currentNumber.toString()
+			);
+			io.to(roomId).emit(
+				'nextCard',
+				nextSuite + '_' + nextNumber.toString()
+			);
+			io.to(roomId).emit(
+				'thirdCard',
+				thirdSuite + '_' + thirdNumber.toString()
+			);
+			players[currentPlayerIndex].score =
+				players[currentPlayerIndex].score + 1;
+			io.to(roomId).emit('turn', players[currentPlayerIndex].name);
+			io.to(roomId).emit('turnScore', rooms[roomId].turnScore);
+			io.to(roomId).emit('deathStack', rooms[roomId].deathStack);
+		} else if ((guess == 'higher') | (guess == 'lower')) {
+			rooms[roomId].currentNumber = nextNumber;
+			rooms[roomId].currentSuite = nextSuite;
+			rooms[roomId].deathStack = 0;
+			rooms[roomId].turnScore = 0;
+			rooms[roomId].nextNumber = Math.floor(Math.random() * 12) + 1;
+			rooms[roomId].nextSuite = getRandomSuite();
+			rooms[roomId].thirdNumber = Math.floor(Math.random() * 12) + 1;
+			rooms[roomId].thirdSuite = getRandomSuite();
+			io.to(roomId).emit('correct', 'false');
+			io.to(roomId).emit(
+				'currentCard',
+				currentSuite + '_' + currentNumber.toString()
+			);
+			io.to(roomId).emit(
+				'nextCard',
+				nextSuite + '_' + nextNumber.toString()
+			);
+			io.to(roomId).emit(
+				'thirdCard',
+				thirdSuite + '_' + thirdNumber.toString()
+			);
+			io.to(roomId).emit('deathStack', 0);
+			io.to(roomId).emit('turnScore', 0);
+			if (currentPlayerIndex == players.length - 1) {
+				rooms[roomId].currentPlayerIndex = 0;
+			} else {
+				rooms[roomId].currentPlayerIndex += 1;
+			}
+
+			io.to(roomId).emit(
+				'turn',
+				players[rooms[roomId].currentPlayerIndex].name
+			);
+		}
+		// Broadcast the message to all connected clients
+		io.emit('guess', guess);
+	});
+
+	socket.on('pass', (turnScore) => {
+		let roomId = socket.roomId;
+		let currentPlayerIndex = rooms[roomId].currentPlayerIndex;
+		let players = rooms[roomId].players;
+		let deathStack = rooms[roomId].deathStack;
+
+		rooms[roomId].deathStack = turnScore;
+		rooms[roomId].turnScore = 0;
+		if (currentPlayerIndex == players.length - 1) {
+			rooms[roomId].currentPlayerIndex = 0;
+		} else {
+			rooms[roomId].currentPlayerIndex += 1;
+		}
+		io.to(roomId).emit('deathStack', deathStack);
+		io.to(roomId).emit('turnScore', 0);
+		io.to(roomId).emit(
+			'turn',
+			players[rooms[roomId].currentPlayerIndex].name
+		);
+	});
+	// Handle disconnection
+	socket.on('disconnect', () => {
+		const roomId = socket.roomId;
+		if (rooms[roomId]) {
+			rooms[roomId].removePlayer(socket.id);
+			io.to(roomId).emit('playerLeft', socket.id);
+		}
+		console.log('A user disconnected');
+	});
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
