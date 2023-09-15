d491bc259c09f00604a90ade62a9903d07b18fb6
Grayson-Remington <graysonbremington@gmail.com>
2023-09-15 16:18:07 -0400

    reverting back

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -70,6 +70,11 @@ const Chat = () => {
 	};
 	const resetCardPosition = async () => {
 		await controls.start({
+			zIndex: 0,
+			transition: { duration: 0.0 },
+		});
+
+		await controls.start({
 			x: 0,
 			y: 0,
 			scale: 1.0,
@@ -79,6 +84,15 @@ const Chat = () => {
 		});
 	};
 	const reset2Cards = async () => {
+		await (controls.start({
+			zIndex: 0,
+			transition: { duration: 0.0 },
+		}),
+		controls2.start({
+			zIndex: 0,
+			transition: { duration: 0.0 },
+		}));
+
 		await (controls.start({
 			x: 0,
 			y: 0,
